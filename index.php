3eedd8ac895711a2ddaa2f05898319bbeff92a4f
Shuto Inoue <shutoi65@gmail.com>
2014-10-05 22:37:58 +0900

    fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ Hello world
 function writeHeader(){
 
 	 $.ajax({
-		url: "header.html", //パスはcommon.jsが読み込まれたHTMLファイルが基準になります
+		url: "sidebar.html", //パスはcommon.jsが読み込まれたHTMLファイルが基準になります
 		     cache: false, //キャッシュを利用するか（お好みで）
 		     	    async: false, //非同期で読み込むか（お好みで）
 			    	   success: function(html){
