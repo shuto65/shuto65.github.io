b6a2e94075351d453ba4d42c203c27a11a5fdf2d
Shuto Inoue <shutoi65@gmail.com>
2014-10-05 22:37:18 +0900

    fix

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
+
+<script type="text/javascript">
+	writeHeader("./"); //先述のサンプルからこの部分だけ変更しています
+</script>
+
+
+
 Hello world
-<?php
-   include 'sidebar.html';
-?>
+
+function writeHeader(){
+
+	 $.ajax({
+		url: "header.html", //パスはcommon.jsが読み込まれたHTMLファイルが基準になります
+		     cache: false, //キャッシュを利用するか（お好みで）
+		     	    async: false, //非同期で読み込むか（お好みで）
+			    	   success: function(html){
+						document.write(html);	
+									}
+									});
+
+}
